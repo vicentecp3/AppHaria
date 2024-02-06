d2c36cd07c04fb0bfbd6edbb23bd6c94b6020df9
Vicente <vcasplaharia@gmail.com>
2024-02-06 08:57:07 +0000

    recuadro
    se hizo el recuadro verde del incio

--- app/src/main/java/com/example/appharia/MainActivity.kt
+++ app/src/main/java/com/example/appharia/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.appharia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,8 +11,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -33,9 +37,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.appharia.ui.theme.AppHariaTheme
 
@@ -50,7 +59,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun Myapp() {
     Surface(
@@ -128,16 +136,36 @@ fun Incio(onContinueClicked: () -> Unit) {
                 )
             },
             content = { innerPadding ->
-                LazyColumn(
-                    contentPadding = innerPadding,
+                Column(
+                   modifier = Modifier.padding(innerPadding),
                     verticalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
-
+                    Row(
+                        Modifier
+                            .background(Color.Green)
+                            .width(400.dp)
+                            .height(200.dp)
+                    ) {
+                        Text(text = "imagenes")
+                    }
                 }
             }
         )
-        Card(Modifier.background(Color.Green)){
-            Text(text = "fdf")
+    }
+}
+
+@Composable
+fun RecuadroCiclos() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val canvasSize = size
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+        rotate(degrees = 90F) {
+            drawRect(
+                color = Color.Gray,
+                topLeft = Offset(x = canvasWidth / 80F, y = canvasHeight / 3F),
+                size = canvasSize / 4F
+            )
         }
     }
 }
